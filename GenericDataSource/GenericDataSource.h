d34004a01b1aa0eeaaa70e5cb12cb4a97c1ae535
Mohamed Afifi <mohamede1945@gmail.com>
2016-02-13 20:25:10 +0100

    Initial Commit

---
+++ GenericDataSource/GenericDataSource.h
@@ -0,0 +1,19 @@
+//
+//  GenericDataSource.h
+//  GenericDataSource
+//
+//  Created by Mohamed Afifi on 2/13/16.
+//  Copyright © 2016 Mohamed Afifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericDataSource.
+FOUNDATION_EXPORT double GenericDataSourceVersionNumber;
+
+//! Project version string for GenericDataSource.
+FOUNDATION_EXPORT const unsigned char GenericDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericDataSource/PublicHeader.h>
+
+
