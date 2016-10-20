d0ba5ce414d07483874e44b98e070f72437633d8
Mohamed Afifi <mohamede1945@gmail.com>
2016-10-20 22:32:29 +0200

    Supplementary view tester

---
+++ GenericDataSourceTests/SupplementaryViewOfKindTester.swift
@@ -0,0 +1,9 @@
+//
+//  SupplementaryViewOfKindTester.swift
+//  GenericDataSource
+//
+//  Created by Mohamed Afifi on 10/20/16.
+//  Copyright © 2016 mohamede1945. All rights reserved.
+//
+
+import Foundation
